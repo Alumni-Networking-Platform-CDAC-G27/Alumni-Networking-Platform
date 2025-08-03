80a53e0385e0afd48c02d4cf75c4155300fd7db0
Rishu Kumar <rishukr0123@gmail.com>
2025-08-03 13:52:30 +0530

    entities modified

--- anp/src/main/java/com/anp/domain/entities/Like.java
+++ anp/src/main/java/com/anp/domain/entities/Like.java
@@ -1,4 +1,47 @@
 package com.anp.domain.entities;
 
-public class Like {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "likes")
+public class Like extends BaseEntity {
+    private Long count = 0L;
+    private User user;
+    private Post post;
+
+    public Like() {
+    }
+
+    @Column(name = "count")
+    public Long getCount() {
+        return this.count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name= "user_id", referencedColumnName = "id")
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne(targetEntity = Post.class)
+    @JoinColumn(name= "post_id", referencedColumnName = "id")
+    public Post getPost() {
+        return this.post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
--- anp/src/main/java/com/anp/domain/entities/UserRole.java
+++ anp/src/main/java/com/anp/domain/entities/UserRole.java
@@ -1,4 +1,25 @@
 package com.anp.domain.entities;
 
-public class UserRole {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import org.springframework.security.core.GrantedAuthority;
+
+
+@Entity
+@Table(name = "roles")
+public class UserRole extends BaseEntity implements GrantedAuthority {
+    private String authority;
+
+    public UserRole() {
+    }
+
+    @Column(name = "authority", nullable = false)
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
 }
