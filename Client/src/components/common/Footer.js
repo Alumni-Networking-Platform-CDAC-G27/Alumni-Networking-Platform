292b68d196da659f20faed17ff9ae9e5627e23ce
rijulj19 <74869775+rijulj19@users.noreply.github.com>
2025-08-05 16:28:02 +0530

    frontend updated

---
+++ Client/src/components/common/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="App-footer">
+        <div className="mb-5">
+            <p className="footer"> &copy; Alumni Networking Platform @ 2025. All rights reserved.</p>
+        </div>
+    </footer>
+);
+
+export default Footer;
